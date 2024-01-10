d25f4769cdb065c590e0434c2aa71b2b3eb899ec
Komil Tokhirov <komil.tokhirov@gmail.com>
2024-01-10 23:48:26 +0400

    added printing of multiple lined paragraph

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,27 @@ async function countToTen()
         await delay(1000);
     }
 }
+
+async function printHello()
+{
+    await delay(1500);
+    document.getElementById("Hello").innerHTML = "Hello!";
+}
+
+async function printMultipleLines()
+{
+    await delay(2000);
+    div = document.getElementById("MultipleLines");
+    div.innerHTML += "<p>";
+    for (let i = 0; i < 5; i++) {
+        div.innerHTML += "Hi!!!<br>";
+    }
+    div.innerHTML += "</p>";
+}
+
+function main()
+{
+    countToTen();
+    printHello();
+    printMultipleLines();
+}
